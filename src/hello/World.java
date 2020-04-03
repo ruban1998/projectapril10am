151c526e95beca16081d2950090cd82a3d3c1513
ruban1998 <ruban10398@gmail.com>
2020-04-03 11:25:55 +0530

    sample selenium code

---
+++ src/hello/World.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class World {
+	public void hel()
+	{
+		System.out.println("hello world");
+	}
+public static void main(String[] args) {
+	World h = new World();
+	h.hel();
+}
+}
